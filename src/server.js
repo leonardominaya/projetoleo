c3c48e0c79854cf27c9ff3c71a1450f0739bae0e
Leonardo Minaya <leonardo.minaya@outlook.com>
2020-06-24 01:22:42 -0300

    leo

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const server = express()
+
+server.use(express.static("public"))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    nocache: true
+})
+
+server.get("/", (req, res) =>{
+    return res.render("index.html", {title: "um titulo"})
+})
+
+server.get("/create-point", (req, res) =>{
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) =>{
+    return res.render("search-results.html")
+})
+
+// server.get("/page-header", (req, res) =>{
+//     return res.render("/partials/page-header.html")
+// })
+
+server.listen(3000)
